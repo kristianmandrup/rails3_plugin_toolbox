e259ad76a95acac492df314410636347a1f8adc2
Kristian Mandrup <kmandrup@gmail.com>
2010-08-22 16:09:51 +0200

    refactored app creation into spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,17 @@ require 'action_controller'
 require 'action_view'
 require 'rails3_plugin_toolbox'
 
+# See http://www.igvita.com/2010/08/04/rails-3-internals-railtie-creating-plugins/
+
+# require 'active_support/railtie'
+require 'rails/all'
+
+module Minimal
+  class Application < Rails::Application
+    config.active_support.deprecation = :log
+  end
+end
+
 module MyAddition
   def zzz
     'zzz'
